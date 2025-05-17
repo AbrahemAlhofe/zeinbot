bd57d3143a939111daed13068ed89ae97c2ec2a9
Abrahem Alhofe <alhofeabrahem88@gmail.com>
2025-05-17 16:17:50 +0300

    inspect vercel function

---
+++ api/index.js
@@ -0,0 +1,18 @@
+import { Hono } from 'hono'
+import { handle } from 'hono/vercel'
+
+const app = new Hono().basePath('/api')
+
+app.get('/', (c) => {
+  return c.json({ message: "Congrats! You've deployed Hono to Vercel" })
+})
+
+const handler = handle((...args) => {
+    console.log(args);
+});
+
+export const GET = handler;
+export const POST = handler;
+export const PATCH = handler;
+export const PUT = handler;
+export const OPTIONS = handler;
